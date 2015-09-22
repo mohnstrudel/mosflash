e17038d69408e43a06fcaaac1c723e36271748be
Anton Kostin <schnibba@gmail.com>
2015-09-22 18:06:52 +0300

    add require yaml

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
+# Initialize the Rails application. Comment our for deploy purposes, because initializing is generating errors
+# (app is already initialized)
 # Rails.application.initialize!
