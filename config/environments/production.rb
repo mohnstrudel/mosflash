f9d398a801c7f3659991f375875a71f853d755d1
Anton Kostin <schnibba@gmail.com>
2016-06-08 20:59:40 +0300

    Action mailer host 2nd attempt.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.default_url_options[:host] = "souvenirnya-produktsiya.ru"
+  config.action_mailer.default_url_options = { host: "souvenirnya-produktsiya.ru" }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USER"],
