7262b1faffdb4c06635b8fe68be1b857b6efefee
Anton Kostin <schnibba@gmail.com>
2016-11-14 21:55:27 +0300

    Add Fleshki and Power Bank to index page.

--- app/controllers/store_front_controller.rb
+++ app/controllers/store_front_controller.rb
@@ -4,6 +4,9 @@ class StoreFrontController < ApplicationController
 		@products = Product.last(8)
 		@hotProducts = Product.hot.order(sorting: :asc)
 		@members = Member.all
+		@fleshki_category = Category.flash[0]
+		@power_bank_category = Category.power_bank[0]
+
 	end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ActiveRecord::Base
 	has_many :subcategories
 
 	mount_uploader :avatar, AvatarUploader
+
+	scope :flash, ->{where(title: 'Флешки')}
+	scope :power_bank, ->{where(title: 'Power Bank')}
 end
